d7665b9b5bc4646316ee4862c52593aeb67aab7c
snovvvvy <zachlathrop84@gmail.com>
2025-09-01 18:09:23 -0400

    update

--- fnaf coop/MainScript.lua
+++ fnaf coop/MainScript.lua
@@ -5,5 +5,7 @@ local placeids = {
 }
 
 if (game.PlaceId == placeids.lobby or game.PlaceId == placeids.lobby2) then 
-    
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/snovvvvy/snow-client/main/fnaf%20coop/Lobby.lua"))()
+elseif (game.PlaceId == placeids.fnaf1) then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/snovvvvy/snow-client/main/fnaf%20coop/fnaf1.lua"))()
 end
