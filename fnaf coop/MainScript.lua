3e22ccb1f521200c237fd77e36def3a5f6f55126
snovvvvy <zachlathrop84@gmail.com>
2025-09-05 19:29:00 -0400

    update

--- fnaf coop/MainScript.lua
+++ fnaf coop/MainScript.lua
@@ -4,9 +4,6 @@ local placeids = {
     fnaf1 = 12497348201
 }
 
-if queue_on_teleport then queue_on_teleport(
-    'loadstring(game:HttpGet("https://raw.githubusercontent.com/snovvvvy/snow-client/refs/heads/main/fnaf%20coop/MainScript.lua"))()'
-) end
 
 if (game.PlaceId == placeids.lobby or game.PlaceId == placeids.lobby2) then 
     loadstring(game:HttpGet("https://raw.githubusercontent.com/snovvvvy/snow-client/main/fnaf%20coop/Lobby.lua"))()
